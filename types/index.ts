da5e566f256ac44a090da2eebb5c54835b5b6d78
0xMrik <agu.aymeric@hotmail.fr>
2023-10-18 20:43:37 +0200

    add hero section

--- types/index.ts
+++ types/index.ts
@@ -3,5 +3,16 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
-    handleClick: MouseEventHandler<HTMLButtonElement>
+    handleClick?: MouseEventHandler<HTMLButtonElement>
+    btnType?: "button" | "submit"
 }
+
+// '?' = 'optionnal'
+// '|' = 'or'
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void
+}
+
+// void = return nothing 
