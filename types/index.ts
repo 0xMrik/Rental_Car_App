72d6b3c121f9c1750a364d5612e4eb74c89291fc
0xMrik <agu.aymeric@hotmail.fr>
2023-10-19 20:24:59 +0200

    add card & modal

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,36 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>
-    btnType?: "button" | "submit"
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 // '?' = 'optionnal'
 // '|' = 'or'
 
 export interface SearchManufacturerProps {
-    manufacturer: string;
-    setManufacturer: (manufacturer: string) => void
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
 }
 
-// void = return nothing 
+// void = return nothing
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
